8080fe1f3902cb75c860d5f9413b885708ec814f
AndrejsAbrickis <andrejs.abrickis@mintos.com>
2018-12-01 12:11:44 +0200

    init repo

---
+++ main.ts
@@ -0,0 +1,34 @@
+/// <reference path="./node_modules/lighthouse/typings/externs.d.ts"/>
+
+import * as fs from 'fs';
+import * as chromeLauncher from 'chrome-launcher';
+import lighthouse from 'lighthouse/lighthouse-core';
+import ReportGenerator from 'lighthouse/lighthouse-core/report/report-generator';
+
+const run = async (url: string, options: chromeLauncher.Options) => {
+  const chrome: chromeLauncher.LaunchedChrome = await chromeLauncher.launch({ chromeFlags: options.chromeFlags });
+  options.port = chrome.port;
+
+  try {
+    const results: LH.RunnerResult = await lighthouse(url, options);
+    const jsonReport = ReportGenerator.generateReport(results.lhr, 'json');
+
+    fs.writeFile('results.json', jsonReport, (err) => {
+      if (err) {
+        console.log(err);
+
+        return;
+      };
+      
+      console.log('Successfully Written to File: results.json');
+    });
+  } catch (error) {
+    throw new Error(error);
+  } finally {
+    await chrome.kill();
+  }
+}
+
+const urlToTest = 'https://www.abrickis.me/';
+
+run(urlToTest, { chromeFlags: ['--headless'] });
